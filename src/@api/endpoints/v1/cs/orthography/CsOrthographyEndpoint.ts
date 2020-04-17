15bdcc2b928cafdc4f9e062220cd046470f3a655
Miloš Havlíček <miloshavlicek@gmail.com>
2020-04-17 12:26:29 +0200

    endpoint fix

--- src/@api/endpoints/v1/cs/orthography/CsOrthographyEndpoint.ts
+++ src/@api/endpoints/v1/cs/orthography/CsOrthographyEndpoint.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { IEndpoint } from "../../../../shared/interfaces/IEndpoint";
 import { AbstractEndpoint } from "../../AbstractEndpoint";
 import { CsLetterTokenizerEndpoint } from "./tokenizer/CsLetterTokenizerEndpoint";
+import { CsSentenceTokenizerEndpoint } from "./tokenizer/CsSentenceTokenizerEndpoint";
 
 export class CsOrthographyEndpoint extends AbstractEndpoint
   implements IEndpoint {
@@ -14,6 +15,9 @@ export class CsOrthographyEndpoint extends AbstractEndpoint
   }
 
   protected appendSubEndpoints(): IEndpoint[] {
-    return [new CsLetterTokenizerEndpoint(this.app, this.getPath())];
+    return [
+      new CsLetterTokenizerEndpoint(this.app, this.getPath()),
+      new CsSentenceTokenizerEndpoint(this.app, this.getPath())
+    ];
   }
 }
